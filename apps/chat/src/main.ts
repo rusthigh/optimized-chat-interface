7813f4b0b1a6d632e0ce571a67d4338e9ac8c556
rusthigh <rusthigh@proton.me>
2021-03-04 15:19:07 +0000

    Cleanup tests

--- apps/chat/src/main.ts
+++ apps/chat/src/main.ts
@@ -6,4 +6,16 @@ import {
 } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { provideHttpCli
+import { provideHttpClient } from '@angular/common/http';
+import { ChatTitleStrategy } from '@ameliorated-chat/chat/feat-chat';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideHttpClient(),
+    {
+      provide: TitleStrategy,
+      useClass: ChatTitleStrategy
+    }
+  ]
+}).catch((err) => console.error(err));
