e44dba00058f4e9ea11c9c31cddc7dfa78a7ce20
rusthigh <rusthigh@proton.me>
2021-03-14 19:15:06 +0000

    Added code manifesto as code of conduct

--- libs/chat/data-access-chat-state/jest.config.ts
+++ libs/chat/data-access-chat-state/jest.config.ts
@@ -7,4 +7,16 @@ export default {
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
-      
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment'
+  ]
+}
