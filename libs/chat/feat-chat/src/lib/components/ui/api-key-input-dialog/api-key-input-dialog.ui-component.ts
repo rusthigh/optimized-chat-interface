f196e9a90fecee3edd5550ae149b0af1ee640c9c
rusthigh <rusthigh@proton.me>
2021-03-23 23:11:54 +0000

    remove old commented code

---
+++ libs/chat/feat-chat/src/lib/components/ui/api-key-input-dialog/api-key-input-dialog.ui-component.ts
@@ -0,0 +1,31 @@
+
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DialogUiComponent } from '@ameliorated-chat/frontend/ui-design-system';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'ac-api-key-input-dialog',
+  standalone: true,
+  imports: [CommonModule, DialogUiComponent, FormsModule],
+  templateUrl: './api-key-input-dialog.ui-component.html',
+  styleUrls: ['./api-key-input-dialog.ui-component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ApiKeyInputDialogUiComponent {
+  @Input() public apiKey = '';
+  @Output() public readonly apiKeyEntered = new EventEmitter<string>();
+  @Output() public readonly closeDialog = new EventEmitter<void>();
+
+  public submitForm(): void {
+    if (this.apiKey.length > 0) {
+      this.apiKeyEntered.emit(this.apiKey);
+    }
+  }
+}
