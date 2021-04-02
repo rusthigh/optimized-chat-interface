ef3a98fdb21afb9e7028227809f61df0f3ed7958
rusthigh <rusthigh@proton.me>
2021-04-02 23:03:31 +0000

    date fix

--- libs/chat/feat-chat/src/lib/components/ui/chat-list/chat-list.ui-component.ts
+++ libs/chat/feat-chat/src/lib/components/ui/chat-list/chat-list.ui-component.ts
@@ -12,4 +12,28 @@ import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { EditableUiComponent } from '@ameliorated-chat/frontend/ui-design-system';
 import { InputState } from '@ameliorated-chat/frontend/util-state';
-import { map, Observable } from '
+import { map, Observable } from 'rxjs';
+
+type ViewModel = {
+  favoritedChats: Chat[];
+  allChats: Chat[];
+  currentChatId: string | null;
+  showEmptyState: boolean;
+};
+
+type ChatListInputState = {
+  chats: Chat[];
+  currentChatId: string | null;
+};
+
+@Component({
+  selector: 'ac-chat-list',
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule, EditableUiComponent],
+  templateUrl: './chat-list.ui-component.html',
+  styleUrls: ['./chat-list.ui-component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChatListUiComponent {
+  @InputState()
+  private readonly inputSta
