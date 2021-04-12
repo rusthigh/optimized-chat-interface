7f2e62df7a667b7e8b577477fba0b7c020b24bb9
rusthigh <rusthigh@proton.me>
2021-04-12 20:52:54 +0000

    init build

--- libs/chat/feat-chat/src/lib/components/ui/chat-message/chat-message.ui-component.ts
+++ libs/chat/feat-chat/src/lib/components/ui/chat-message/chat-message.ui-component.ts
@@ -10,4 +10,28 @@ import { Message, Role } from '@ameliorated-chat/chat/type-chat';
 import { InputState } from '@ameliorated-chat/frontend/util-state';
 import { map, Observable } from 'rxjs';
 
-type ChatM
+type ChatMessageInputState = {
+  message: Message | null;
+  userProfilePicUrl: string;
+};
+
+type ViewModel = {
+  message: string;
+  avatarUrl: string;
+  isUserMessage: boolean;
+  isSystemMessage: boolean;
+  isAssistantMessage: boolean;
+  isDefaultUserIcon: boolean;
+};
+
+@Component({
+  selector: 'ac-chat-message',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chat-message.ui-component.html',
+  styleUrls: ['./chat-message.ui-component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChatMessageUiComponent {
+  @InputState()
+  private r
