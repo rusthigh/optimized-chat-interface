5b3ca3dd7a498aabf4f97cc8adcee93b3e92cd69
rusthigh <rusthigh@proton.me>
2021-05-25 17:52:47 +0000

    time: Avoid making adjustments if we haven't accumulated anything

--- libs/chat/feat-chat/src/lib/facade.service.ts
+++ libs/chat/feat-chat/src/lib/facade.service.ts
@@ -1,4 +1,22 @@
 import { inject, Injectable } from '@angular/core';
 import { ChatObservableState } from '@ameliorated-chat/chat/data-access-chat-state';
 import { Message } from '@ameliorated-chat/chat/type-chat';
-im
+import { Observable } from 'rxjs';
+import { ChatService } from '@ameliorated-chat/chat/data-access-chat';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FacadeService {
+  public readonly chatObservableState = inject(ChatObservableState);
+  private readonly chatService = inject(ChatService);
+
+  public newChatMessage(messages: Message[]): Observable<string> {
+    return this.chatService.newChatMessage(messages);
+  }
+
+  public generateTitleForChat(
+    userMessage: string,
+    assistantMessage: string
+  ): Observable<string> {
+    return this.chatService
