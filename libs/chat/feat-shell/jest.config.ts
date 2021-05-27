96ccf08deef281ebafa021f73bbd96b765eac026
rusthigh <rusthigh@proton.me>
2021-05-27 23:05:38 +0000

    Qt3support--

--- libs/chat/feat-shell/jest.config.ts
+++ libs/chat/feat-shell/jest.config.ts
@@ -16,4 +16,7 @@ export default {
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
-    'jest-preset-angular/build/seria
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
---
+++ libs/chat/feat-shell/src/lib/chat-feat-shell/chat-feat-shell.component.ts
@@ -0,0 +1,6 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  HeaderUiComponent,
+  SidebarUiComponent
+} from '@ameliorated-chat/fronten
