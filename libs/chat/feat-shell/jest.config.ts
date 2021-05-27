1fb1323ca306f7f093a265103385f75670cb9dea
rusthigh <rusthigh@proton.me>
2021-05-27 06:15:58 +0000

    add me

--- libs/chat/feat-shell/jest.config.ts
+++ libs/chat/feat-shell/jest.config.ts
@@ -1,4 +1,19 @@
 /* eslint-disable */
 export default {
   displayName: 'chat-feat-shell',
-  pr
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../../coverage/libs/chat/feat-shell',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/seria
