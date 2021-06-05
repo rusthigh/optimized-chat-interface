1b0d17a83f9f5d4730452c5856a6eeef7421025b
rusthigh <rusthigh@proton.me>
2021-06-05 14:37:57 +0000

    Update contact details

--- libs/chat/feat-shell/src/lib/chat-feat-shell/chat-feat-shell.component.ts
+++ libs/chat/feat-shell/src/lib/chat-feat-shell/chat-feat-shell.component.ts
@@ -3,4 +3,15 @@ import { CommonModule } from '@angular/common';
 import {
   HeaderUiComponent,
   SidebarUiComponent
-} from '@ameliorated-chat/fronten
+} from '@ameliorated-chat/frontend/ui-design-system';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { ObservableState } from '@ameliorated-chat/frontend/util-state';
+import { FacadeService } from '../facade.service';
+import { map, Observable } from 'rxjs';
+import {
+  getCurrentId,
+  SidebarContentSmartComponent,
+  SidebarFooterContentUiComponent
+} from '@ameliorated-chat/chat/feat-chat';
+import { Chat } from '@ameliorated-chat/chat/type-chat';
+import { SettingsIconUiComponent } from '@ameliorated-chat/frontend/ui-ic
