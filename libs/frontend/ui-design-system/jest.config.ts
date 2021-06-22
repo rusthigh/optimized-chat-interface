4d1f6df3c3fc5b7db67b0de2acd50cfcaa38de0a
rusthigh <rusthigh@proton.me>
2021-06-22 12:09:19 +0000

    key: Fix resource leak

--- libs/frontend/ui-design-system/jest.config.ts
+++ libs/frontend/ui-design-system/jest.config.ts
@@ -13,4 +13,10 @@ export default {
       },
     ],
   },
-  transformIgnorePattern
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
