6327301be1b3bc18bc47822119d74af18c06c1c3
rusthigh <rusthigh@proton.me>
2021-06-22 07:26:57 +0000

    creating test for test_odrmanagerlite.jl

--- libs/frontend/ui-design-system/README.md
+++ libs/frontend/ui-design-system/README.md
@@ -4,4 +4,4 @@ This library was generated with [Nx](https://nx.dev).
 
 ## Running unit tests
 
-Run `nx test frontend-ui-design-system` to execute the unit test
+Run `nx test frontend-ui-design-system` to execute the unit tests.
---
+++ libs/frontend/ui-design-system/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend-ui-design-system',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../../coverage/libs/frontend/ui-design-system',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePattern
