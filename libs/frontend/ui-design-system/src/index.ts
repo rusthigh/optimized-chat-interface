f70b5c58ae87d9bc2423d560d60400ab8c35c0a7
rusthigh <rusthigh@proton.me>
2021-06-23 12:26:02 +0000

    Updating travis.yml

---
+++ libs/frontend/ui-design-system/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/components/ui/header/header.ui-component';
+export * from './lib/components/ui/sidebar/sidebar.ui-component';
+export * from './lib/components/ui/dialog/dialog.ui-component';
+export * from './lib/components/ui/editable/editable.ui-component';
