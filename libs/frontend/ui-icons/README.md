3d3233a8fa73865acf728e78c216d17c42812d73
rusthigh <rusthigh@proton.me>
2021-07-11 08:52:01 +0000

    more fixes...

---
+++ libs/frontend/ui-icons/README.md
@@ -0,0 +1,8 @@
+
+# frontend-ui-icons
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test frontend-ui-icons` to execute the unit tests.
---
+++ libs/frontend/ui-icons/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend-ui-icons',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../../coverage/libs/frontend/ui-icons',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
+  },
+  tr
