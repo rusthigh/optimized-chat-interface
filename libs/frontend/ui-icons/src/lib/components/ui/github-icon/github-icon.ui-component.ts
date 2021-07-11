55c52a88c2495dd4d77df7e9a6236063ab297c77
rusthigh <rusthigh@proton.me>
2021-07-11 14:59:02 +0000

    USB: serial: ftdi_sio: add id for Brainboxes serial cards

---
+++ libs/frontend/ui-icons/src/lib/components/ui/github-icon/github-icon.ui-component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'svg[acGithubIcon]',
+  standalone: true,
+  template: `
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24">
+      <path
+     
