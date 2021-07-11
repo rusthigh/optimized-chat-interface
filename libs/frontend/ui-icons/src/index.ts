42924dee6aafcf07f845fecab6c652b86b447c0c
rusthigh <rusthigh@proton.me>
2021-07-11 14:05:03 +0000

    Pontoon: Update Georgian (ka) localization of AMO Frontend

---
+++ libs/frontend/ui-icons/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/components/ui/github-icon/github-icon.ui-component';
+export * from './lib/components/ui/twitter-icon/twitter-icon.ui-component';
+export * from './lib/components/ui/openai-icon/openai-icon.ui-component';
+export * 
