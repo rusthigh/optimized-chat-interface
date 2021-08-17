d00e7b97bc632ec55916aa4d182e0f522c165441
rusthigh <rusthigh@proton.me>
2021-08-17 07:53:51 +0000

    Updated translation from Launchpad

---
+++ libs/frontend/util-local-storage/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+  displayName: 'frontend-util-local-storage',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  coverageDirectory: '../../../coverage/libs/frontend/util-local-storage',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+   
