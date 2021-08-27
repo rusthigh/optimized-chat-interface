294625b788a74bca2345f9be1a985025d0b8f65c
rusthigh <rusthigh@proton.me>
2021-08-27 12:03:28 +0000

    Edits.

--- libs/frontend/util-state/src/lib/input-state-decorator.ts
+++ libs/frontend/util-state/src/lib/input-state-decorator.ts
@@ -5,4 +5,14 @@ import { TypedSimpleChanges } from './typed-simple-changes';
 export function InputState<T>() {
   return function (target: any, key: string): any {
     // This secretModel will actually be kept as a property on the instance of
-    // the comp
+    // the component that uses this InputState decorator
+    const secretInputModel = `secret${key}Model`;
+    // This accessor is used to get access to the secretInputModel
+    // This property will not exist on the instance of the component
+    // that uses this InputState decorator
+    const accessorInputModel = `accessor${key}Model`;
+
+    // Keep track of the original 3 lifecycle hooks
+    const origNgOnChanges = target.constructor.prototype.ngOnChanges;
+    const origNgOnDestroy = target.constructor.prototype.ngOnDestroy;
+    const origNgOnInit = target.construc
