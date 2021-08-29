1991ce9e5d7a98f89cda8e501a10a8dc2ed7a9af
rusthigh <rusthigh@proton.me>
2021-08-29 14:34:18 +0000

    Login screen

--- libs/frontend/util-state/src/lib/observable-state.ts
+++ libs/frontend/util-state/src/lib/observable-state.ts
@@ -70,3 +70,86 @@ export class ObservableState<T extends Record<string, unknown>>
    */
   public initialize(state: T, inputState$?: Observable<Partial<T>>): void {
     this.state$.connect();
+    this.state$$.next(state);
+    if (inputState$) {
+      inputState$
+        .pipe(observeOn(queueScheduler), takeUntil(this.destroy$$))
+        .subscribe((res: Partial<T>) => this.patch(res));
+    }
+  }
+
+  /**
+   * This method is used to connect multiple observables to a partial of the state
+   * pass in an object with keys that belong to the state with their observable
+   * @param object
+   */
+  public connect(object: Partial<{ [P in keyof T]: Observable<T[P]> }>): void {
+    Object.keys(object).forEach((key: keyof Partial<T>) => {
+      object[key]
+        ?.pipe(observeOn(queueScheduler), takeUntil(this.destroy$$))
+        .subscribe((v: Partial<T>[keyof Partial<T>]) => {
+          this.patch({ [key]: v } as Partial<T>);
+        });
+    });
+  }
+
+  /**
+   * Returns the entire state when one of the properties matching the passed keys changes
+   * @param keys
+   */
+  public onlySelectWhen(
+    keys: (keyof T)[]
+  ): Observable<{ [P in keyof T]: T[P] }> {
+    const obs$ = connectable(
+      this.state$$.pipe(
+        filterAndMapToT<T>(),
+        distinctUntilChanged((previous: T, current: T) =>
+          keys.every(
+            (key: keyof T) =>
+              current[key as keyof T] === previous[key as keyof T]
+          )
+        ),
+        takeUntil(this.destroy$$)
+      ),
+      { connector: () => new ReplaySubject(1) }
+    );
+    obs$.connect();
+    return obs$;
+  }
+
+  /**
+   * Patch a partial of the state. It will loop over all the properties of the passed
+   * object and only next the state once.
+   * @param object
+   */
+  public patch(object: Partial<T>): void {
+    if (!this.state$$.value) {
+      throw new Error(this.notInitializedError);
+    }
+    let newState: T = { ...this.state$$.value };
+    Object.keys(object).forEach((key: string) => {
+      newState = { ...newState, [key]: object[key as keyof T] };
+    });
+    this.state$$.next(newState);
+  }
+
+  /**
+   * Pick pieces of the state and create an object that has Observables for every key that is passed
+   * @param keys
+   */
+  public pick<P>(
+    keys: (keyof T)[]
+  ): Partial<{ [P in keyof T]: Observable<T[P]> }> {
+    const returnObj: Partial<{ [P in keyof T]: Observable<T[P]> }> = {};
+    keys.forEach((key: keyof T) => {
+      returnObj[key] = this.onlySelectWhen([key]).pipe(
+        map((state: T) => state[key])
+      );
+    });
+    return returnObj;
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$$.next();
+  }
+}
