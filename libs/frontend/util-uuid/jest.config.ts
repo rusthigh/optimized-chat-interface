b09f565a9615b5cdd5de76ad6108b12c2468f4d2
rusthigh <rusthigh@proton.me>
2021-09-18 08:18:32 +0000

    Update process.php

--- libs/frontend/util-uuid/jest.config.ts
+++ libs/frontend/util-uuid/jest.config.ts
@@ -17,4 +17,6 @@ export default {
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
     'jest-preset-angular/build/serializers/ng-snapshot',
-    'jest-preset-angular/build/serializers/html-c
+    'jest-preset-angular/build/serializers/html-comment'
+  ]
+};
